0614791100ad90b36867649e11fc0556ec676d7d
Jakub Burda <jakubburdaofc@gmail.com>
2025-01-26 14:31:34 +0100

    chore(store): register joke reducer in Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,18 @@
 // Importi Redux Toolkit to simplify the Redux store configuration
 import { configureStore } from '@reduxjs/toolkit';
 
+// Import reducers to manage different parts of the application state
+import jokeReducer from './slices/jokeSlices';
+
 /**
  * Configures the Redux store with the specified reducer logic.
  * Uses Redux Toolkit's `configureStore` to automatically set up the store with useful defaults.
  * @returns {Object} The configured Redux store.
  */
 const store = configureStore({
-  // reducers: {},
+    reducer: {
+        joke: jokeReducer,
+      },
 });
 
 export default store;

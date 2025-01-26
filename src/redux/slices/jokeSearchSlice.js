3456912ee883617de18dda17fa034eb84ffe8a20
Jakub Burda <jakubburdaofc@gmail.com>
2025-01-26 14:39:31 +0100

    feat(slice): initialize joke serach slice with state, actions and reducer

---
+++ src/redux/slices/jokeSearchSlice.js
@@ -0,0 +1,38 @@
+// Import Redux Toolkit's createSlice to simplify state management
+import { createSlice } from '@reduxjs/toolkit';
+
+// Initial state for the joke search slice
+const initialState = {
+  query: '',
+  result: '',
+  isLoading: false,
+  error: null,
+};
+
+// Create the joke search slice
+const jokeSearchSlice = createSlice({
+  name: 'jokeSearch',
+  initialState,
+  reducers: {
+    // Action to set the search query
+    setSearchQuery(state, action) {
+      state.query = action.payload;
+    },
+    // Action to set the search result
+    setSearchResult(state, action) {
+      state.result = action.payload;
+    },
+    // Action to set the loading state
+    setSearchLoading(state, action) {
+      state.isLoading = action.payload;
+    },
+    // Action to set any error in searching
+    setSearchError(state, action) {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setSearchQuery, setSearchResult, setSearchLoading, setSearchErro } = jokeSearchSlice.actions;
+
+export default jokeSearchSlice.reducer;

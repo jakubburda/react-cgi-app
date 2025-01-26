ef71756ba3fd50f708efb8899c2a84a3fb807b70
Jakub Burda <jakubburdaofc@gmail.com>
2025-01-26 17:59:50 +0100

    feat(router): set up base routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-function App() {
+// React hooks and core functionality
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Components
+import JokeCard from './components/JokeCard';
+
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <h1>Chuck Norris Jokes</h1>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<JokeCard />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;

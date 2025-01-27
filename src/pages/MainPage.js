392b94aa081125f9ec1e47cada921324fefc71c7
Jakub Burda <jakubburdaofc@gmail.com>
2025-01-27 09:45:44 +0100

    feat(JokeCategoryMenu): implement JokeCategoryMenu to page with switch logic

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,38 @@
+// React hooks and external libraries 
+import { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { setSelectedCategory } from '../redux/slices/categorySlice';
+
 // Components
-import JokeCard from "../components/JokeCard";
 import Title from "../components/elements/Title";
+import JokeCard from "../components/modules/JokeCard";
+import JokeCategoryMenu from '../components/menus/JokeCategoryMenu';
 
 const MainPage = () => {
+  const dispatch = useDispatch();
+  const [activeComponent, setActiveComponent] = useState("JokeCard"); 
+
   return (
     <>
       <Title />
-      <JokeCard />
+
+      {/* Control logic to change the component */}
+      <div style={{ marginBottom: "20px" }}>
+        <button onClick={() => setActiveComponent("JokeCategoryMenu")}>
+          Kategories
+        </button>
+
+        <button onClick={() => {
+          setActiveComponent("JokeCard");
+          dispatch(setSelectedCategory(null));
+        }}>
+          Random
+        </button>
+      </div>
+
+      {/* Logic for displaying a component based on its state */}
+      {activeComponent === "JokeCategoryMenu" && <JokeCategoryMenu />}
+      {activeComponent === "JokeCard" && <JokeCard mode="random" />}
     </>
   );
 };
